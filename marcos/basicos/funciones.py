253fe8c1c286d3177cd29b69ed3e6c4e92b46ad8
mnceleiro <markiusarria@gmail.com>
2014-02-10 11:53:30 +0100

    Añadidos ejercicios de listas dadas en clase de teoria. Tambien se han renombrado carpetas para que sean reconocidas de forma mas intuitiva

---
+++ marcos/basicos/funciones.py
@@ -0,0 +1,27 @@
+# fns
+# LIBRO DIVE INTO PYTHON O INVERSION EN PYTHON
+# IDES: wingide
+
+# funcion doble
+def doble(x):
+	return x * 2
+
+print(doble(5))
+
+#############
+
+def iniciaPar():
+	return 5, 6
+
+def foo():
+	pass	# Funcion que no hace nada
+
+a, b = iniciaPar()
+print (doble (5))
+print(a)
+print(b)
+foo()
+
+#############
+
+
