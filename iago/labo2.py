eb46607dc398a928e6316e647c1091959e0260d1
ifcunharro <ifcunharro@gmail.com>
2014-02-13 20:45:48 +0100

    Corregido error al pasar expresión corta

--- iago/labo2.py
+++ iago/labo2.py
@@ -54,37 +54,39 @@ llamada recursiva con el indice donde comienza el nuevo operador encontrado, lo 
 para el segundo operando, teniendo en cuenta que comienza una posicion a la 
 derecha del primer operando'''
 def evaluar(cad,indice):
-	op = ['+','-','*','/']
-	resul = ''
-	op1 = ''
-	op2 = ''
-	indiceInicial = indice
-	lista = cad.split()
-	try:
-		if lista[indice] in op:
-			operator = lista[indice]
-			indice+=1;
-			if lista[indice].isdigit():
-				op1 = lista[indice]
-				indice+=1
+	if len(cad) >= 5:
+		op = ['+','-','*','/']
+		resul = ''
+		op1 = ''
+		op2 = ''
+		indiceInicial = indice
+		lista = cad.split()
+		try:
+			if lista[indice] in op:
+				operator = lista[indice]
+				indice+=1;
+				if lista[indice].isdigit():
+					op1 = lista[indice]
+					indice+=1
+				else:
+					op1, indice = evaluar(cad,indice)		
+				if lista[indice].isdigit():
+					op2 = lista[indice]
+				else:
+					op2,indice = evaluar(cad,indice)
+				resul += str(float(eval(op1+operator+op2)))
+				if indiceInicial == 0:
+					print("El resultado de evaluar "+cad+" es {0:.2f}".format(float(resul)))
+				else:
+					return resul, indiceInicial+3
 			else:
-				op1, indice = evaluar(cad,indice)		
-			if lista[indice].isdigit():
-				op2 = lista[indice]
-			else:
-				op2,indice = evaluar(cad,indice)
-			aux = len(op1)+len(operator)+len(op2)
-			resul += str(float(eval(op1+operator+op2)))
-			if indiceInicial == 0:
-				print("El resultado de evaluar "+cad+" es {0:.2f}".format(float(resul)))
-			else:
-				return resul, indiceInicial+aux
-		else:
-			raise Exception("Operador erroneo encontrado: "+lista[indice])
-	except TypeError:
-		pass
-	except Exception as e:
-		print(e)
+				raise Exception("Operador erroneo encontrado: "+lista[indice])
+		except TypeError:
+			pass
+		except Exception as e:
+			print(e)
+	else:
+		print("Expresion demasiado corta para ser evaluada")
 		
 		
 	
@@ -101,75 +103,83 @@ llamada recursiva con el indice donde comienza el nuevo operador encontrado, lo 
 para el segundo operando, teniendo en cuenta que comienza una posicion a la 
 izquierda del primer operando'''
 def evaluarPost(cad,indice):
-	op = ['+','-','*','/']
-	resul = ''
-	op1 = ''
-	op2 = ''
-	indiceInicial = indice
-	lista = cad.split()
-	try:
-		if lista[indice] in op:
-			operator = lista[indice]
-			indice-=1
-			if lista[indice].isdigit():
-				op1 = lista[indice]
+	if len(cad) >= 5:
+		op = ['+','-','*','/']
+		resul = ''
+		op1 = ''
+		op2 = ''
+		indiceInicial = indice
+		lista = cad.split()
+		try:
+			if lista[indice] in op:
+				operator = lista[indice]
 				indice-=1
+				if lista[indice].isdigit():
+					op1 = lista[indice]
+					indice-=1
+				else:
+					op1, indice = evaluarPost(cad,indice)
+				if lista[indice].isdigit():
+					op2 = lista[indice]
+				else:
+					op2,indice = evaluarPost(cad,indice)
+				resul += str(float(eval(op1+operator+op2)))
+				if indiceInicial == -1:
+					print("El resultado de evaluar "+cad+" es {0:.2f}".format(float(resul)))
+				else:
+					return resul, indiceInicial-3
 			else:
-				op1, indice = evaluarPost(cad,indice)
-			if lista[indice].isdigit():
-				op2 = lista[indice]
-			else:
-				op2,indice = evaluarPost(cad,indice)
-			resul += str(float(eval(op1+operator+op2)))
-			if indiceInicial == -1:
-				print("El resultado de evaluar "+cad+" es {0:.2f}".format(float(resul)))
-			else:
-				return resul, indiceInicial-3
-		else:
-			raise Exception("Operador erroneo encontrado: "+lista[indice])
-	except TypeError:
-		pass
-	except Exception as e:
-		print(e)
+				raise Exception("Operador erroneo encontrado: "+lista[indice])
+		except TypeError:
+			pass
+		except Exception as e:
+			print(e)
+	else:
+		print("Expresion demasiado corta para ser evaluada")
+
 		
 evaluarPost('1 3 4 + +',-1)
 
 #Solucion labo2 ejercicio 2 función que permite evaluar tanto notación prefija como postfija
 '''Si los indices son negativos se trata de postfija y si son positivos se trata de prefija'''
 def evaluarPreAndPost(cad,indice):
-	op = ['+','-','*','/']
-	resul = ''
-	op1 = ''
-	op2 = ''
-	indiceInicial = indice
-	lista = cad.split()
-	try:
-		if lista[indice] in op:
-			operator = lista[indice]
-			indice = preOrPostIndice(indice)
-			if lista[indice].isdigit():
-				op1 = lista[indice]
+	if len(cad) >= 5:
+		op = ['+','-','*','/']
+		resul = ''
+		op1 = ''
+		op2 = ''
+		indiceInicial = indice
+		lista = cad.split()
+		try:
+			if lista[indice] in op:
+				operator = lista[indice]
 				indice = preOrPostIndice(indice)
-			else:
-				op1, indice = evaluarPreAndPost(cad,indice)
-			if lista[indice].isdigit():
-				op2 = lista[indice]
-			else:
-				op2,indice = evaluarPreAndPost(cad,indice)
-			resul += str(float(eval(op1+operator+op2)))
-			if indiceInicial == -1 or indiceInicial == 0:
-				print("El resultado de evaluar "+cad+" es {0:.2f}".format(float(resul)))
-			else:
-				if indiceInicial >= 0:
-					return resul, preOrPostInicial(indiceInicial)
+				if lista[indice].isdigit():
+					op1 = lista[indice]
+					indice = preOrPostIndice(indice)
 				else:
-					return resul, preOrPostInicial(indiceInicial)
-		else:
-			raise Exception("Operador erroneo encontrado: "+lista[indice])
-	except TypeError:
-		pass
-	except Exception as e:
-		print(e)
+					op1, indice = evaluarPreAndPost(cad,indice)
+				if lista[indice].isdigit():
+					op2 = lista[indice]
+				else:
+					op2,indice = evaluarPreAndPost(cad,indice)
+				resul += str(float(eval(op1+operator+op2)))
+				if indiceInicial == -1 or indiceInicial == 0:
+					print("El resultado de evaluar "+cad+" es {0:.2f}".format(float(resul)))
+				else:
+					if indiceInicial >= 0:
+						return resul, preOrPostInicial(indiceInicial)
+					else:
+						return resul, preOrPostInicial(indiceInicial)
+			else:
+				raise Exception("Operador erroneo encontrado: "+lista[indice])
+		except TypeError:
+			pass
+		except Exception as e:
+			print(e)
+	else:
+		print("Expresion demasiado corta para ser evaluada")
+
 
 #Funciones auxiliares de la función evaluarPreAndPost
 '''revisa si el indice x pasado como argumento es un indice
@@ -195,4 +205,7 @@ def preOrPostInicial(x):
 evaluarPreAndPost('/ - 1 3 * 4 6',0)
 
 #prueba función evaluarPreAndPost con expresión postfija
-evaluarPreAndPost('1 3 4 * +',-1)
+evaluarPreAndPost('1 3 4 + +',-1)
+
+#prueba función evaluarPreAndPost con expresión corta
+evaluarPreAndPost('1 +',-1)
