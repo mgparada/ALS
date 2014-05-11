cb831e67be3a4d7cb4a0459b8f843dcafe2652a0
mgparada <maurogomezparada@gmail.com>
2014-05-11 14:14:06 +0200

    deleting project

--- mauro/project/google_appengine/celsiusFarenheit/lib.py
+++
@@ -1,5 +0,0 @@
-def gradosFarenheitCelsius(c):
-	return (c - 32) / 1,8
-
-def gradosCelsiusFarenheit(x):
-	return (x * 9/5) + 32
--- mauro/project/google_appengine/lib/docker/docker/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import (
-    compare_version, convert_port_bindings, mkbuildcontext, ping, tar
-) # flake8: noqa
